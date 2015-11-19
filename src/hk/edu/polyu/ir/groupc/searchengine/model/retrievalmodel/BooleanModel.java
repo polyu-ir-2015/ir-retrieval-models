53077f7f43dcc73c99bfbd9d37ad06f3573c3dab
tonylai16 <tonylai16@yahoo.com.hk>
2015-11-19 16:12:47 +0800

    modified boolean

--- src/hk/edu/polyu/ir/groupc/searchengine/model/retrievalmodel/BooleanModel.java
+++ src/hk/edu/polyu/ir/groupc/searchengine/model/retrievalmodel/BooleanModel.java
@@ -21,89 +21,51 @@ import java.util.function.Consumer;
  * Created by beenotung on 11/12/15.
  */
 public class BooleanModel extends RetrievalModel {
-    public static final int MODE_AND = 2;
-    public static final int MODE_OR = 1;
-    public static final int MODE_DEFAULT = MODE_OR;
-    final int mode;
-
+  
     public BooleanModel() {
-        mode = MODE_DEFAULT;
+       
     }
 
-    public BooleanModel(int mode) {
-        this.mode = mode;
-    }
-
-    public SearchResult search_and(Query query, int numResult) {
-        //TODO implement and
+    public List<RetrievalDocument> search(Query query) {
         List<RetrievalDocument> list = new ArrayList<>();
-        for (Option<TermEntity> termEntityOption : query.simpleTerms()) {
-            if (termEntityOption.isDefined()) {
-                TermEntity termEntity = termEntityOption.get();
-                ScalaSupport.foreachMap(termEntity.filePositionMap(), new Consumer<Tuple2<Object, ArrayBuffer<Object>>>() {
-                    @Override
-                    public void accept(Tuple2<Object, ArrayBuffer<Object>> pair) {
-                        list.add(new RetrievalDocument((Integer) pair._1(), 1));
-                    }
-                });
-            }
+        ArrayList<Integer> int_list = new ArrayList<Integer>();
+        for (ExpandedTerm termEntity : query.expandedTerms()) {
+			int weight = termEntity.weight()>0?1:((termEntity.weight()<0)?-1:0);
+			switch (weight){
+				case -1: 
+						ScalaSupport.foreachMap(termEntity.term().filePositionMap(), new Consumer<Tuple2<Object, ArrayBuffer<Object>>>() {
+							@Override
+							public void accept(Tuple2<Object, ArrayBuffer<Object>> pair) {
+							   if (int_list.contains((Integer) pair._1()))
+								   int_list.remove((Integer) pair._1());
+							}
+						});
+				break; 
+				case 0:
+						ScalaSupport.foreachMap(termEntity.term().filePositionMap(), new Consumer<Tuple2<Object, ArrayBuffer<Object>>>() {
+							@Override
+							public void accept(Tuple2<Object, ArrayBuffer<Object>> pair) {
+								if (!int_list.contains((Integer) pair._1()))
+									int_list.add((Integer) pair._1());
+							}
+						});
+				break;
+				case 1:
+						ScalaSupport.foreachMap(termEntity.term().filePositionMap(), new Consumer<Tuple2<Object, ArrayBuffer<Object>>>() {
+							@Override
+							public void accept(Tuple2<Object, ArrayBuffer<Object>> pair) {
+								if (!int_list.contains((Integer) pair._1()))
+									int_list.remove((Integer) pair._1());
+							   // list.add(new RetrievalDocument((Integer) pair._1(), 1));
+							}
+						});
+				break;
+			}
+       
         }
-        return SearchResultFactory.create(query, list);
-    }
-
-    private void addToList(HashMap<Object, ArrayBuffer<Object>> map, List<RetrievalDocument> list, double score) {
-        ScalaSupport.foreachMap(map, new Consumer<Tuple2<Object, ArrayBuffer<Object>>>() {
-            @Override
-            public void accept(Tuple2<Object, ArrayBuffer<Object>> pair) {
-                list.add(new RetrievalDocument((Integer) pair._1(), score));
-            }
-        });
-    }
-
-    public SearchResult search_ext(Query query, int numResult) {
-        List<RetrievalDocument> result_list = new ArrayList<>();
-
-        List<RetrievalDocument> and_list = new ArrayList<>();
-        List<RetrievalDocument> or_list = new ArrayList<>();
-        List<RetrievalDocument> not_list = new ArrayList<>();
-        for (ExpandedTerm expandedTerm : query.expandedTerms()) {
-            if (expandedTerm.weight() > 0) {
-                addToList(expandedTerm.term().filePositionMap(), and_list, 1);
-            } else if (expandedTerm.weight() < 0) {
-                addToList(expandedTerm.term().filePositionMap(), not_list, 1);
-            } else {
-                addToList(expandedTerm.term().filePositionMap(), or_list, 1);
-            }
+        for (int docID : int_list){
+        	list.add(new RetrievalDocument(docID, 1));
         }
-        //TODO merge list
-
-        return SearchResultFactory.create(query, result_list);
-    }
-
-    public SearchResult search_or(Query query, int numResult) {
-        List<RetrievalDocument> list = new ArrayList<>();
-        for (Option<TermEntity> termEntityOption : query.simpleTerms()) {
-            if (termEntityOption.isDefined()) {
-                TermEntity termEntity = termEntityOption.get();
-                ScalaSupport.foreachMap(termEntity.filePositionMap(), new Consumer<Tuple2<Object, ArrayBuffer<Object>>>() {
-                    @Override
-                    public void accept(Tuple2<Object, ArrayBuffer<Object>> pair) {
-                        list.add(new RetrievalDocument((Integer) pair._1(), 1));
-                    }
-                });
-            }
-        }
-        return SearchResultFactory.create(query, list);
-    }
-
-    @Override
-    public SearchResult search(Query query, int numResult) {
-        switch (mode) {
-            case MODE_AND:
-                return search_or(query, numResult);
-            case MODE_OR:
-            default:
-                return search_and(query, numResult);
-        }
+        return list;
     }
 }
