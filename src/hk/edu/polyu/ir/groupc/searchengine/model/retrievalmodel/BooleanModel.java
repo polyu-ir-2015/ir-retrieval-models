64349ef7238d6b5c5203fb78ac02672e16f0b881
Beeno Tung <aabbcc1241@yahoo.com.hk>
2015-11-23 17:43:46 +0800

    updated search-engine reference; updated retrieval model implement

--- src/hk/edu/polyu/ir/groupc/searchengine/model/retrievalmodel/BooleanModel.java
+++ src/hk/edu/polyu/ir/groupc/searchengine/model/retrievalmodel/BooleanModel.java
@@ -1,19 +1,14 @@
 package hk.edu.polyu.ir.groupc.searchengine.model.retrievalmodel;
 
 import comm.lang.ScalaSupport;
-import hk.edu.polyu.ir.groupc.searchengine.model.datasource.SearchResult;
-import hk.edu.polyu.ir.groupc.searchengine.model.datasource.SearchResultFactory;
-import hk.edu.polyu.ir.groupc.searchengine.model.datasource.TermEntity;
 import hk.edu.polyu.ir.groupc.searchengine.model.query.ExpandedTerm;
 import hk.edu.polyu.ir.groupc.searchengine.model.query.Query;
-import hk.edu.polyu.ir.groupc.searchengine.model.query.RetrievalDocument;
-import hk.edu.polyu.ir.groupc.searchengine.model.query.RetrievalModel;
-import scala.Option;
+import hk.edu.polyu.ir.groupc.searchengine.model.result.RetrievalDocument;
 import scala.Tuple2;
 import scala.collection.mutable.ArrayBuffer;
-import scala.collection.mutable.HashMap;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.function.Consumer;
 
@@ -21,50 +16,96 @@ import java.util.function.Consumer;
  * Created by beenotung on 11/12/15.
  */
 public class BooleanModel extends RetrievalModel {
-  
-    public BooleanModel() {
-       
+    private static final String MODE_OR = "OR";
+    private static final String MODE_AND = "AND";
+    static final String MODE_DEFAULT = MODE_AND;
+    private static final List<String> MODES;
+    private static final List<Parameter<? extends Number>> Parameters;
+    private static final int NOT = -1;
+    private static final int AND = 1;
+    private static final int OR = 0;
+
+
+    static {
+        MODES = new LinkedList<>();
+        MODES.add(MODE_AND);
+        MODES.add(MODE_OR);
+        Parameters = new LinkedList<>();
+    }
+
+    String mode = MODE_DEFAULT;
+
+    @Override
+    public List<String> getModes() {
+        return MODES;
+    }
+
+    @Override
+    public String getDefaultMode() {
+        return MODE_DEFAULT;
+    }
+
+    @Override
+    public String getMode() {
+        return mode;
+    }
+
+    @Override
+    public void setMode(String newMode) {
+        mode = newMode;
+    }
+
+    @Override
+    public List<Parameter<? extends Number>> getParameters() {
+        return Parameters;
     }
 
     public List<RetrievalDocument> search(Query query) {
         List<RetrievalDocument> list = new ArrayList<>();
         ArrayList<Integer> int_list = new ArrayList<Integer>();
         for (ExpandedTerm termEntity : query.expandedTerms()) {
-			int weight = termEntity.weight()>0?1:((termEntity.weight()<0)?-1:0);
-			switch (weight){
-				case -1: 
-						ScalaSupport.foreachMap(termEntity.term().filePositionMap(), new Consumer<Tuple2<Object, ArrayBuffer<Object>>>() {
-							@Override
-							public void accept(Tuple2<Object, ArrayBuffer<Object>> pair) {
-							   if (int_list.contains((Integer) pair._1()))
-								   int_list.remove((Integer) pair._1());
-							}
-						});
-				break; 
-				case 0:
-						ScalaSupport.foreachMap(termEntity.term().filePositionMap(), new Consumer<Tuple2<Object, ArrayBuffer<Object>>>() {
-							@Override
-							public void accept(Tuple2<Object, ArrayBuffer<Object>> pair) {
-								if (!int_list.contains((Integer) pair._1()))
-									int_list.add((Integer) pair._1());
-							}
-						});
-				break;
-				case 1:
-						ScalaSupport.foreachMap(termEntity.term().filePositionMap(), new Consumer<Tuple2<Object, ArrayBuffer<Object>>>() {
-							@Override
-							public void accept(Tuple2<Object, ArrayBuffer<Object>> pair) {
-								if (!int_list.contains((Integer) pair._1()))
-									int_list.remove((Integer) pair._1());
-							   // list.add(new RetrievalDocument((Integer) pair._1(), 1));
-							}
-						});
-				break;
-			}
-       
+            int weight = termEntity.weight() > 0 ? AND : ((termEntity.weight() < 0) ? NOT : OR);
+            /* distinct mode from AND or OR */
+            if (weight != NOT) {
+                if (mode.equals(MODE_AND))
+                    weight = AND;
+                else
+                    weight = OR;
+            }
+            switch (weight) {
+                case NOT:
+                    ScalaSupport.foreachMap(termEntity.term().filePositionMap(), new Consumer<Tuple2<Object, ArrayBuffer<Object>>>() {
+                        @Override
+                        public void accept(Tuple2<Object, ArrayBuffer<Object>> pair) {
+                            if (int_list.contains((Integer) pair._1()))
+                                int_list.remove((Integer) pair._1());
+                        }
+                    });
+                    break;
+                case OR:
+                    ScalaSupport.foreachMap(termEntity.term().filePositionMap(), new Consumer<Tuple2<Object, ArrayBuffer<Object>>>() {
+                        @Override
+                        public void accept(Tuple2<Object, ArrayBuffer<Object>> pair) {
+                            if (!int_list.contains((Integer) pair._1()))
+                                int_list.add((Integer) pair._1());
+                        }
+                    });
+                    break;
+                case AND:
+                    ScalaSupport.foreachMap(termEntity.term().filePositionMap(), new Consumer<Tuple2<Object, ArrayBuffer<Object>>>() {
+                        @Override
+                        public void accept(Tuple2<Object, ArrayBuffer<Object>> pair) {
+                            if (!int_list.contains((Integer) pair._1()))
+                                int_list.remove((Integer) pair._1());
+                            // list.add(new RetrievalDocument((Integer) pair._1(), 1));
+                        }
+                    });
+                    break;
+            }
+
         }
-        for (int docID : int_list){
-        	list.add(new RetrievalDocument(docID, 1));
+        for (int docID : int_list) {
+            list.add(new RetrievalDocument(docID, 1));
         }
         return list;
     }
