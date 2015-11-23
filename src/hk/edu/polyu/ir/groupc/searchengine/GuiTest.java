56d4c010104d69aa4067ddb08a07d12646b2f5a3
Beeno Tung <aabbcc1241@yahoo.com.hk>
2015-11-23 17:48:48 +0800

    added boolean model to gui

--- src/hk/edu/polyu/ir/groupc/searchengine/GuiTest.java
+++ src/hk/edu/polyu/ir/groupc/searchengine/GuiTest.java
@@ -2,10 +2,7 @@ package hk.edu.polyu.ir.groupc.searchengine;
 
 import hk.edu.polyu.ir.groupc.searchengine.frontend.MainApplication;
 import hk.edu.polyu.ir.groupc.searchengine.frontend.MainController;
-import hk.edu.polyu.ir.groupc.searchengine.model.retrievalmodel.ExtendedBooleanModel;
-import hk.edu.polyu.ir.groupc.searchengine.model.retrievalmodel.RetrievalModel;
-import hk.edu.polyu.ir.groupc.searchengine.model.retrievalmodel.SetBasedVectorSpaceModel;
-import hk.edu.polyu.ir.groupc.searchengine.model.retrievalmodel.VectorSpaceModel;
+import hk.edu.polyu.ir.groupc.searchengine.model.retrievalmodel.*;
 
 import java.util.ArrayList;
 
@@ -16,8 +13,9 @@ public class GuiTest {
     public static void main(String [] args){
         ArrayList<RetrievalModel> models = MainController.MODELS();
         /* clear origin models, e.g. simple model */
-//        models.clear();
+        models.clear();
         /* register models */
+        models.add(new BooleanModel());
         models.add(new VectorSpaceModel());
         models.add(new SetBasedVectorSpaceModel());
         models.add(new ExtendedBooleanModel());
